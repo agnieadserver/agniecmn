09fb1adf91f4a162f84db7b9e150e044a60cda58
pranoti <devnull@localhost>
2012-07-04 06:34:03 +0530

    Suppressed some warning messages.

--- code/common/util/src/main/java/com/agnie/common/util/validator/EMailValidator.java
+++ code/common/util/src/main/java/com/agnie/common/util/validator/EMailValidator.java
@@ -11,6 +11,7 @@ import java.util.regex.Pattern;
 @Constraint(EMail.class)
 public class EMailValidator extends Validator {
 
+	@SuppressWarnings("unused")
 	private EMail	eMailConstraint;
 
 	public EMailValidator(Annotation constraint) {
@@ -23,7 +24,6 @@ public class EMailValidator extends Validator {
 
 	@Override
 	public boolean validate(String value) {
-		// TODO Auto-generated method stub
 
 		if (value != null) {
 			Pattern pattern = Pattern.compile("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");

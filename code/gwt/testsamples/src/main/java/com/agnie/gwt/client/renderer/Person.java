57d58369550a1c971131188427ce7cbb01aa3d91
pranoti <devnull@localhost>
2012-07-24 09:21:18 +0530

    Working sample of UiRenderer. ref #25.

---
+++ code/gwt/testsamples/src/main/java/com/agnie/gwt/client/renderer/Person.java
@@ -0,0 +1,70 @@
+package com.agnie.gwt.client.renderer;
+
+public class Person {
+
+	private String	fname;
+	private String	lname;
+	private String	emailid;
+	private int		age;
+
+	/**
+	 * @return the fname
+	 */
+	public String getFname() {
+		return fname;
+	}
+
+	/**
+	 * @param fname
+	 *            the fname to set
+	 */
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	/**
+	 * @return the lname
+	 */
+	public String getLname() {
+		return lname;
+	}
+
+	/**
+	 * @param lname
+	 *            the lname to set
+	 */
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	/**
+	 * @return the emailid
+	 */
+	public String getEmailid() {
+		return emailid;
+	}
+
+	/**
+	 * @param emailid
+	 *            the emailid to set
+	 */
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ code/gwt/testsamples/src/main/java/com/agnie/gwt/client/renderer/RendererSample.java
@@ -0,0 +1,46 @@
+package com.agnie.gwt.client.renderer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.ui.Composite;
+
+public class RendererSample extends Composite {
+
+	CellList<Person>	cellList;
+
+	public RendererSample() {
+		PearsonCell cell = new PearsonCell();
+		cellList = new CellList<Person>(cell);
+		initWidget(cellList);
+		init();
+	}
+
+	private void init() {
+		List<Person> list = new ArrayList<Person>();
+		Person p = new Person();
+		p.setFname("Pranoti");
+		p.setLname("Patil");
+		p.setEmailid("pranoti.patil@gmail.com");
+		p.setAge(30);
+		list.add(p);
+
+		p = new Person();
+		p.setFname("Pandurang");
+		p.setLname("Patil");
+		p.setEmailid("pandurang.patil@gmail.com");
+		p.setAge(30);
+		list.add(p);
+
+		p = new Person();
+		p.setFname("Ravi");
+		p.setLname("Kumar");
+		p.setEmailid("ravi.kumar@gmail.com");
+		p.setAge(30);
+		list.add(p);
+
+		cellList.setRowCount(list.size(), true);
+		cellList.setRowData(list);
+	}
+}

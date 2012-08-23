9aaaff1469e55b44276406808e7ee07ef50659b7
sureshheble <devnull@localhost>
2012-08-23 06:18:43 +0530

    Ceated landing html page from given image

--- code/gwt/testsamples/src/main/java/com/agnie/gwt/client/samples.java
+++ code/gwt/testsamples/src/main/java/com/agnie/gwt/client/samples.java
@@ -2,15 +2,21 @@ package com.agnie.gwt.client;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-import com.agnie.gwt.client.renderer.UserRenderer;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import com.agnie.gwt.client.shared.Person;
+import com.agnie.gwt.client.ui.ValidatePersonView;
 import com.agnie.gwt.common.client.widget.Account;
+import com.agnie.gwt.common.client.widget.BreadCrumbPanel;
 import com.agnie.gwt.common.client.widget.DecoratedPanel;
 import com.agnie.gwt.common.client.widget.LabelPasswordBox;
 import com.agnie.gwt.common.client.widget.LabelTextBox;
-import com.agnie.gwt.common.client.widget.LocaleListBox;
 import com.agnie.gwt.common.client.widget.MessagePanel;
-import com.agnie.gwt.common.client.widget.MessagePanel.MessageType;
 import com.agnie.gwt.common.client.widget.PageTitle;
 import com.agnie.gwt.common.client.widget.SearchBox;
 import com.agnie.gwt.common.client.widget.SearchBoxResources;
@@ -18,11 +24,7 @@ import com.agnie.gwt.common.client.widget.StyledListBox;
 import com.agnie.gwt.common.client.widget.SuggestionBox;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -53,60 +55,87 @@ public class samples implements EntryPoint {
 	Account		acc1=new Account();
 	Account		acc2=new Account();
 	User us = new User();
+	BreadCrumbPanel		bcp=new BreadCrumbPanel();
 	private static SearchBoxResources	resource	= SearchBoxResources.INSTANCE;
+	Person p=new Person();
+	private ValidatorFactory factory = Validation.byDefaultProvider().configure().buildValidatorFactory();
+	
 
 	public void onModuleLoad() {
 		GWT.log("IN onmoduleLoad Start.");
-		// RendererSample rendSample = new RendererSample();
-		UserRenderer ur = new UserRenderer();
-		for (int i = 0; i < 5; i++) {
-			celllist.add("sug" + i);
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<Person>> violations = validator.validate(p);
+		
+		for (ConstraintViolation<Person> constraintViolation : violations) {
+			System.out.println("message=="+constraintViolation.getMessage());
+			System.out.println("InvalidValue=="+constraintViolation.getInvalidValue());
+			System.out.println("MessageTmplate=="+constraintViolation.getMessageTemplate());
+			System.out.println("ConstraintDescriptor=="+constraintViolation.getConstraintDescriptor());
+			System.out.println("PropertyPath=="+constraintViolation.getPropertyPath());
+			
 		}
-		sb.setData(celllist);
-		sb.setWidth("300px");
-		sb.setHeight("300px");
-		dp = new DecoratedPanel("Decorated Panel");
-
-		dp.addContent(save);
-		dp.addContent(tb);
-		dp.closeClickHandler(new ClickHandler() {
-
-			public void onClick(ClickEvent event) {
-				dp.getParent().removeFromParent();
-			}
-		});
-		ltb.setLabel("username");
-		lpb.setLabel("password");
-		GWT.log("In Entry point ltb.getLabel==" + ltb.getLabel() + "ltb.getText()==" + ltb.getText());
-		save.addClickHandler(new ClickHandler() {
-
-			public void onClick(ClickEvent event) {
-				GWT.log("In Entry point save ltb.getLabel==" + ltb.getLabel() + "ltb.getText()==" + ltb.getText());
-			}
-		});
-		mpi.setType(MessageType.INFORMATION);
-		mpi.setMessage("Information: infromation information height set nothing");
-		// mpi.setHeight("20px");
-		mpe.setType(MessageType.ERROR);
-		mpe.setMessage("Error:error error height set to 40px");
-		mpe.setHeight("40px");
-		mpw.setType(MessageType.WARNING);
-		mpw.setMessage("Warning:warning warning height set to 60 px");
-		mpw.setHeight("60px");
-
-		searchBox.setSize("200px", "20px");
-		//searchBox.setLabel("suresh");
-		img.setUrl("images/search.png");
-		img.addClickHandler(new ClickHandler() {
-
-			public void onClick(ClickEvent event) {
-				Window.alert("You are finding " + searchBox.getValue());
-			}
-		});
-		searchBox.addInputWidget(img);
-
-		pt.setPageTitle("Title for page1");
-		pt.addTitleImage(img);
+//		img.setUrl("images/search.png");
+//		bcp.addBreadCrumb("BreadCrumb1",img);
+//		bcp.addBreadCrumb("BreadCrumb2");
+//		bcp.addBreadCrumb("BreadCrumb3");
+//		for(int i=0;i<bcp.getBreadCrumbCount();i++){
+//		GWT.log("breadcrumbcount=="+bcp.getBreadCrumbCount());
+//		GWT.log(bcp.getBreadCrumbText(i));
+//		
+//		}
+//		int i=bcp.getBreadCrumbCount()-1;
+//		GWT.log("last node index=="+i);
+//		RootPanel.get().add(bcp);
+		// RendererSample rendSample = new RendererSample();
+//		UserRenderer ur = new UserRenderer();
+//		for (int i = 0; i < 5; i++) {
+//			celllist.add("sug" + i);
+//		}
+//		sb.setData(celllist);
+//		sb.setWidth("300px");
+//		sb.setHeight("300px");
+//		dp = new DecoratedPanel("Decorated Panel");
+//
+//		dp.addContent(save);
+//		dp.addContent(tb);
+//		dp.closeClickHandler(new ClickHandler() {
+//
+//			public void onClick(ClickEvent event) {
+//				dp.getParent().removeFromParent();
+//			}
+//		});
+//		ltb.setLabel("username");
+//		lpb.setLabel("password");
+//		GWT.log("In Entry point ltb.getLabel==" + ltb.getLabel() + "ltb.getText()==" + ltb.getText());
+//		save.addClickHandler(new ClickHandler() {
+//
+//			public void onClick(ClickEvent event) {
+//				GWT.log("In Entry point save ltb.getLabel==" + ltb.getLabel() + "ltb.getText()==" + ltb.getText());
+//			}
+//		});
+//		mpi.setType(MessageType.INFORMATION);
+//		mpi.setMessage("Information: infromation information height set nothing");
+//		// mpi.setHeight("20px");
+//		mpe.setType(MessageType.ERROR);
+//		mpe.setMessage("Error:error error height set to 40px");
+//		mpe.setHeight("40px");
+//		mpw.setType(MessageType.WARNING);
+//		mpw.setMessage("Warning:warning warning height set to 60 px");
+//		mpw.setHeight("60px");
+//
+//		searchBox.setSize("200px", "20px");
+//		//searchBox.setLabel("suresh");
+//		img.setUrl("images/search.png");
+//		img.addClickHandler(new ClickHandler() {
+//
+//			public void onClick(ClickEvent event) {
+//				Window.alert("You are finding " + searchBox.getValue());
+//			}
+//		});
+//		searchBox.addInputWidget(img);
+//
+//		pt.setPageTitle("Title for page1");
+//		pt.addTitleImage(img);
 		
 		// RootPanel.get().add(ltb);
 		// RootPanel.get().add(save);
@@ -187,12 +216,12 @@ public class samples implements EntryPoint {
 //		RootPanel.get().add(vselunPanel);
 		// ListBoxt Test -- end
 		acc.setAccName("UserName");
-		acc1.setAccName("UserName1"+"Username1");
-		acc2.setAccName("UserName2"+"UserName22222");
+//		acc1.setAccName("UserName1"+"Username1");
+//		acc2.setAccName("UserName2"+"UserName22222");
 		dp1.add(acc);
 //		dp1.add(acc1);
 //		dp1.add(acc2);
-		
-		RootPanel.get().add(dp1);
+//		
+//		RootPanel.get().add(dp1);
 	}
 }

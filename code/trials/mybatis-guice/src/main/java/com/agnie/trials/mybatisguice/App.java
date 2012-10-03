d548571fbb9ad8ba7b8348bac09b15b817c19541
pranoti <devnull@localhost>
2012-10-03 12:05:09 +0530

    module to try mybatis-guice. ref #63

---
+++ code/trials/mybatis-guice/src/main/java/com/agnie/trials/mybatisguice/App.java
@@ -0,0 +1,13 @@
+package com.agnie.trials.mybatisguice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

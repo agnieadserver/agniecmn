0c45254d0c5f50767244ce05b0c0305ad9a385b9
pranoti <devnull@localhost>
2012-12-09 18:22:03 +0530

    Removed Singleton annotation.

--- code/common/test-common/src/main/java/com/agnie/common/test/providers/ACLCtxManager.java
+++ code/common/test-common/src/main/java/com/agnie/common/test/providers/ACLCtxManager.java
@@ -1,14 +1,12 @@
 package com.agnie.common.test.providers;
 
 import com.agnie.common.server.auth.ACLContext;
-import com.google.inject.Singleton;
 
 /**
  * This class should not be instantiated directly and used. It should be always need to used by getting it injected from
  * Guice injector.
  * 
  */
-@Singleton
 public class ACLCtxManager {
 
 	private ACLContext	context;

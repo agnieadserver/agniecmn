df7c1e07908e70a61c3b8bcc03a611d50182c652
pranoti <devnull@localhost>
2013-02-01 09:03:40 +0530

    Added some javadoc

--- code/common/commons/src/main/java/com/agnie/common/gwt/serverclient/client/enums/Language.java
+++ code/common/commons/src/main/java/com/agnie/common/gwt/serverclient/client/enums/Language.java
@@ -4,6 +4,12 @@ import java.io.Serializable;
 
 import com.agnie.common.gwt.serverclient.client.renderer.Title;
 
+/**
+ * There is a corresponding Language enum in Persistence client module. If you make some changes here you need to also
+ * copy the same changes over there also. We had to maintain these enums separately as there is an issue in
+ * mvn-helper-plugin which generates RequestFactory interfaces.
+ * 
+ */
 public enum Language implements Serializable, Title {
 	MARATHI("mr", "मराठी"), HINDI("hi", "हिन्दी"), ENGLISH("en", "English");
 	private String	code;

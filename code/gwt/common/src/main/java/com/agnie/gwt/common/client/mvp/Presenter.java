1f4070bfa809de0c842a41a970b1083697b2127e
sureshheble <devnull@localhost>
2013-05-22 09:57:03 +0530

    Changed return type to boolean from void for Presneter interface(com.agnie.gwt.common.client.mvp)

--- code/gwt/common/src/main/java/com/agnie/gwt/common/client/mvp/Presenter.java
+++ code/gwt/common/src/main/java/com/agnie/gwt/common/client/mvp/Presenter.java
@@ -1,7 +1,7 @@
 package com.agnie.gwt.common.client.mvp;
 
 public abstract interface Presenter {
-	public abstract void go();
+	public abstract boolean go();
 
 	public abstract void postRender(MainView view);
 

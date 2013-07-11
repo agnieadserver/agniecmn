6dfe76f5ed48b2b72adb0b728422689fb5507ee4
pranoti <devnull@localhost>
2013-07-11 16:14:36 +0530

    made the changes to fix issue #179 from 3a4users

--- code/common/commons/src/main/java/com/agnie/common/gwt/serverclient/client/enums/QueryString.java
+++ code/common/commons/src/main/java/com/agnie/common/gwt/serverclient/client/enums/QueryString.java
@@ -2,7 +2,7 @@ package com.agnie.common.gwt.serverclient.client.enums;
 
 public enum QueryString {
 	SOURCE("source"), SESSION("sessionid"), GWT_DEV_MODE("gwt.codesvr"), LOCALE("locale"), QUESTION_MARK("?"), AMPERSAND("&"), HASH("#"), USER_NAME("user"), DOMAIN("domain"), SELECTED_DOMAIN(
-			"sel-domain"), HISTORY_HASH("history-token"), SELECTED_CONTEXT("sel-context"), CONTEXT("context");
+			"sel-domain"), HISTORY_HASH("history-token"), SELECTED_CONTEXT("sel-context"), CONTEXT("context"), API_ACCESS_KEY("access-key");
 
 	private String	key;
 

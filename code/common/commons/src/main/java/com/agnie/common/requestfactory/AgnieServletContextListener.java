1d463d20ed8c4a05707d7c1d6226638480597931
pandurangpatil <root@agnie.co.in>
2014-02-13 17:07:13 +0530

    Created common context listener to inject guice injector in servlet context so that it is accessible inside jsp.

---
+++ code/common/commons/src/main/java/com/agnie/common/requestfactory/AgnieServletContextListener.java
@@ -0,0 +1,31 @@
+package com.agnie.common.requestfactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+/**
+ * Super Guice context listener to inject Guice injector inside servlet context so that it can be accessed inside jsp.
+ * 
+ * @author Pandurang Patil 13-Feb-2014
+ * 
+ */
+public abstract class AgnieServletContextListener extends GuiceServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		// No call to super as it also calls getInjector()
+		ServletContext sc = servletContextEvent.getServletContext();
+		sc.setAttribute(Injector.class.getName(), getInjector());
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		ServletContext sc = servletContextEvent.getServletContext();
+		sc.removeAttribute(Injector.class.getName());
+		super.contextDestroyed(servletContextEvent);
+	}
+
+}

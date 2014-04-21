4bdf56066f2848d2644c76fd9fcfae324e9b32c5
pandurangpatil <root@agnie.co.in>
2014-04-21 06:55:55 +0530

    added copyright header.

--- code/gwt/testsamples/src/main/java/com/agnie/gwt/client/renderer/User.java
+++ code/gwt/testsamples/src/main/java/com/agnie/gwt/client/renderer/User.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * © 2014 Copyright Agnie Technologies
+ *   
+ * NOTICE: All information contained herein is, and remains the property of Agnie Technologies and its suppliers, if
+ * any. The intellectual and technical concepts contained herein are proprietary to Agnie Technologies and its suppliers
+ * and may be covered by Indian and Foreign Patents, patents in process, and are protected by trade secret or copyright
+ * law. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Agnie Technologies.
+ ******************************************************************************/
 package com.agnie.gwt.client.renderer;
 
 public class User {

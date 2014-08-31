cd843d3fae832bc2bd9503049aead443fc26c51f
pandurangpatil <root@agnie.co.in>
2014-08-31 10:25:33 +0530

    Added custom radio and check box widgets. ref #14.

---
+++ code/gwt/bootstrap-common/src/main/java/com/agnie/gwt/bootstrap/proto/admin/client/ui/CustomRadioType.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * © 2014 Copyright Agnie Technologies
+ *  
+ *  NOTICE: All information contained herein is, and remains the property of Agnie Technologies and its suppliers, if
+ *  any. The intellectual and technical concepts contained herein are proprietary to Agnie Technologies and its suppliers
+ *  and may be covered by Indian and Foreign Patents, patents in process, and are protected by trade secret or copyright
+ *  law. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ *  permission is obtained from Agnie Technologies.
+ ******************************************************************************/
+package com.agnie.gwt.bootstrap.proto.admin.client.ui;
+
+/**
+ * @author Pandurang Patil 28-Aug-2014
+ *
+ */
+public enum CustomRadioType {
+
+	PRIMARY("radio-primary"), SUCCESS("radio-success"), WARNING("radio-warning"), DANGER("radio-danger"), INFO("radio-info"), DEFAULT("radio-default");
+
+	private final String	type;
+
+	private CustomRadioType(final String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+}

f0cac19351cdf696f8fb15bca17995850b123970
pandurangpatil <root@agnie.co.in>
2014-10-24 16:00:37 +0530

    Updated copyright message and tested all projects are getting built properly except mvptest and war-manager as those projects were giving errors even before this change. This fixes #16

--- code/trials/jpatest/src/main/java/com/test/PersistModule.java
+++ code/trials/jpatest/src/main/java/com/test/PersistModule.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * © 2014 Copyright AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ *     
+ * NOTICE: All information contained herein is, and remains the property of AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers, if any. 
+ * The intellectual and technical concepts contained herein are proprietary to AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers and 
+ * may be covered by Indian and Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this information 
+ * or reproduction of this material is strictly forbidden unless prior written permission is obtained from AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ ******************************************************************************/
 package com.test;
 
 import com.google.inject.AbstractModule;

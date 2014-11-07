0c9463c2f9c0c8dd1c86e56ee57d470b0c00d7cb
pandurangpatil <pandurang@agnie.net>
2014-11-07 22:33:11 +0530

    Added common long manager to be used in test environment.

---
+++ code/common/test-common/src/main/java/com/agnie/common/test/providers/LongManager.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * © 2014 Copyright AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ *     
+ * NOTICE: All information contained herein is, and remains the property of AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers, if any. 
+ * The intellectual and technical concepts contained herein are proprietary to AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers and 
+ * may be covered by Indian and Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this information 
+ * or reproduction of this material is strictly forbidden unless prior written permission is obtained from AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ ******************************************************************************/
+package com.agnie.common.test.providers;
+
+/**
+ * @author Pandurang Patil 10-Feb-2014
+ * 
+ */
+public class LongManager {
+
+	private Long	value;
+
+	/**
+	 * @return the value
+	 */
+	public Long getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *            the value to set
+	 */
+	public void setValue(Long value) {
+		this.value = value;
+	}
+
+}
---
+++ code/common/test-common/src/main/java/com/agnie/common/test/providers/LongProvider.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * © 2014 Copyright AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ *     
+ * NOTICE: All information contained herein is, and remains the property of AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers, if any. 
+ * The intellectual and technical concepts contained herein are proprietary to AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers and 
+ * may be covered by Indian and Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this information 
+ * or reproduction of this material is strictly forbidden unless prior written permission is obtained from AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ ******************************************************************************/
+package com.agnie.common.test.providers;
+
+import com.google.inject.Provider;
+
+/**
+ * 
+ * This provider has been specifically been created to facilitate setting required Long value for testing purpose. This
+ * class shouldn't be used in main code. Usage is limited to single threaded environment. If its being used in multi
+ * threaded environment then behaviour is not guaranteed.
+ * 
+ * @author Pandurang Patil 10-Feb-2014
+ * 
+ */
+public class LongProvider implements Provider<Long> {
+
+	private LongManager	mgr;
+
+	public LongProvider(LongManager mgr) {
+		this.mgr = mgr;
+	}
+
+	public Long get() {
+		return mgr.getValue();
+	}
+
+}

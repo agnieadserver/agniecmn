e3186d15c72e6cbc7055a9a8bdb1cf50fe1acda8
pandurangpatil <pandurang@agnie.net>
2015-01-08 14:48:44 +0530

    AS-392 #comment added logout handler.

--- code/gwt/bootstrap-common/src/main/java/com/agnie/gwt/bootstrap/proto/admin/client/ui/AccountsI.java
+++ code/gwt/bootstrap-common/src/main/java/com/agnie/gwt/bootstrap/proto/admin/client/ui/AccountsI.java
@@ -1,16 +1,18 @@
 package com.agnie.gwt.bootstrap.proto.admin.client.ui;
 
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.IsWidget;
 
 public interface AccountsI extends IsWidget {
 
-	public void setName(String name);
+	void setName(String name);
 
-	public void setProfileImage(String profileURL);
+	void setProfileImage(String profileURL);
 
-	public void setMyProfileUrl(String myProfileURL);
+	void setMyProfileUrl(String myProfileURL);
 
-	public void setUpdatePasswordURL(String updatePasswordURL);
+	void setUpdatePasswordURL(String updatePasswordURL);
 
-	public void setLogoutUrl(String urlLogout);
+	HandlerRegistration addLogoutHandler(ClickHandler handler);
 }

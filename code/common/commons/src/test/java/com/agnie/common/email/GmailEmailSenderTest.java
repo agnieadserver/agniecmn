ad9a206f6cfdd27742b02ce143c96cb72af658fb
Saurabh Sameer <saurabh@agnie.net>
2015-12-03 17:08:36 +0530

    AS-869 #comment mail support added. Changes in class GmailSender

--- code/common/commons/src/test/java/com/agnie/common/email/GmailEmailSenderTest.java
+++ code/common/commons/src/test/java/com/agnie/common/email/GmailEmailSenderTest.java
@@ -11,6 +11,8 @@ package com.agnie.common.email;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.mail.Message;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -26,27 +28,44 @@ import com.google.inject.name.Named;
 @WithModules({ CommonModule.class, TestModule.class })
 public class GmailEmailSenderTest {
 
-	@Inject
-	@Named(CommonModule.GMAIL_SENDER)
-	private MailSender			mailSender;
+    @Inject
+    @Named(CommonModule.GMAIL_SENDER)
+    private MailSender          mailSender;
 
-	@Inject
-	@Named(TestModule.TEST_TEMPLATE)
-	private MessageTemplate		msgTpl;
+    @Inject
+    @Named(TestModule.TEST_TEMPLATE)
+    private MessageTemplate     msgTpl;
 
-	private static final String	MSG_SUB	= "Junit Test";
+    private static final String MSG_SUB = "Junit Test";
 
-	@Test
-	public void sendMailTest() {
-		try {
-			Map<String, String> variables = new HashMap<String, String>();
-			variables.put("test", "Junit");
+    @Test
+    public void sendMailTest() {
+        try {
+            Map<String, String> variables = new HashMap<String, String>();
+            variables.put("test", "Junit");
 
-			mailSender.sendMail(new Email("verifier", new Recipient("mail@agnie.co.in"), MSG_SUB, msgTpl.getMessage(variables)));
-			Assert.assertTrue(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.assertTrue(false);
-		}
-	}
+            mailSender.sendMail(new Email("verifier", new Recipient("mail@agnie.co.in"), MSG_SUB, msgTpl.getMessage(variables)));
+            Assert.assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void sendMailTestWithBCC() {
+        try {
+            Map<String, String> variables = new HashMap<String, String>();
+            variables.put("test", "Junit");
+
+            Recipient recipient = new Recipient("mail@agnie.co.in");
+            recipient.put(Message.RecipientType.BCC, "saurabh@agnie.net");
+
+            mailSender.sendMail(new Email("verifier", recipient, MSG_SUB, msgTpl.getMessage(variables)));
+            Assert.assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+    }
 }

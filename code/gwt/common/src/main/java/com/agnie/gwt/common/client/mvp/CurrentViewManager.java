56ddb314834837c1a0e392cb348d2005164511fe
pandurangpatil <pandurang@agnie.net>
2016-05-06 15:37:19 +0530

    PIV-2 #comment made changes to have current view manager which can be injected anywhere and access current view.

---
+++ code/gwt/common/src/main/java/com/agnie/gwt/common/client/mvp/CurrentViewManager.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * © 2014 Copyright AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ *     
+ * NOTICE: All information contained herein is, and remains the property of AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers, if any. 
+ * The intellectual and technical concepts contained herein are proprietary to AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers and 
+ * may be covered by Indian and Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this information 
+ * or reproduction of this material is strictly forbidden unless prior written permission is obtained from AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ ******************************************************************************/
+package com.agnie.gwt.common.client.mvp;
+
+import com.google.inject.Singleton;
+
+/**
+ * @author Pandurang Patil 06-May-2016
+ *
+ */
+@Singleton
+public class CurrentViewManager {
+	MainView	currentView;
+
+	/**
+	 * @return the currentView
+	 */
+	public final MainView getCurrentView() {
+		return currentView;
+	}
+
+	/**
+	 * @param currentView
+	 *            the currentView to set
+	 */
+	public final void setCurrentView(MainView currentView) {
+		this.currentView = currentView;
+	}
+
+}
---
+++ code/gwt/common/src/main/java/com/agnie/gwt/common/client/mvp/ProgressbarView.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * © 2014 Copyright AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ *     
+ * NOTICE: All information contained herein is, and remains the property of AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers, if any. 
+ * The intellectual and technical concepts contained herein are proprietary to AGNIE MEDIA SOFTWARE PRIVATE LIMITED and its suppliers and 
+ * may be covered by Indian and Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this information 
+ * or reproduction of this material is strictly forbidden unless prior written permission is obtained from AGNIE MEDIA SOFTWARE PRIVATE LIMITED.
+ ******************************************************************************/
+package com.agnie.gwt.common.client.mvp;
+
+/**
+ * @author Pandurang Patil 06-May-2016 - 1:49:09 pm
+ *
+ */
+public interface ProgressbarView {
+    void showProgress();
+
+    void hideProgress();
+}

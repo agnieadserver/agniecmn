9abea36a39d1ea130f7e69e4d46b8ba48add42bf
Nilesh <nilesh@agnie.net>
2021-04-20 22:12:41 +0530

    add query strings

--- code/common/commons/src/main/java/com/agnie/common/gwt/serverclient/client/enums/QueryString.java
+++ code/common/commons/src/main/java/com/agnie/common/gwt/serverclient/client/enums/QueryString.java
@@ -9,20 +9,21 @@
 package com.agnie.common.gwt.serverclient.client.enums;
 
 public enum QueryString {
-	SOURCE("source"), SESSION("sessionid"), GWT_DEV_MODE("gwt.codesvr"), LOCALE("locale"), QUESTION_MARK("?"), AMPERSAND("&"), HASH("#"), USER_NAME("user"), DOMAIN("domain"), SELECTED_DOMAIN(
-			"sel-domain"), HISTORY_HASH("history-token"), SELECTED_CONTEXT("sel-context"), CONTEXT("context"), API_ACCESS_KEY("access-key"), REFERER("Referer");
+    SOURCE("source"), SESSION("sessionid"), GWT_DEV_MODE("gwt.codesvr"), LOCALE("locale"), QUESTION_MARK("?"), AMPERSAND("&"), HASH("#"), USER_NAME("user"), DOMAIN("domain"), SELECTED_DOMAIN(
+            "sel-domain"), HISTORY_HASH("history-token"), SELECTED_CONTEXT(
+                    "sel-context"), CONTEXT("context"), API_ACCESS_KEY("access-key"), REFERER("Referer"), HEADER_AUTH_IDENTIFIER("auth_identifier"), HEADER_AUTH_KEY("auth_key");
 
-	private String	key;
+    private String key;
 
-	private QueryString(String key) {
-		this.key = key;
-	}
+    private QueryString(String key) {
+        this.key = key;
+    }
 
-	/**
-	 * @return the key
-	 */
-	public String getKey() {
-		return key;
-	}
+    /**
+     * @return the key
+     */
+    public String getKey() {
+        return key;
+    }
 
 }
